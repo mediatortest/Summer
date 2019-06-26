34672ab60330d7a8d3d123cebed459ff1dafbd0e
wanghaiyang <wanghaiyang@btte.net>
2019-06-26 11:12:02 +0800

    Empty project

---
+++ MainProject/MainProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainProject
+//
+//  Created by 王海洋 on 2019/6/26.
+//  Copyright © 2019 王海洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
